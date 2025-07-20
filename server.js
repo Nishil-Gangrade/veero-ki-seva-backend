39edf576f8988fe72993d02a3953ad2c46a62571
Nishil Gangrade <nishilgangrade2003@gmail.com>
2025-07-20 19:21:07 +0530

    api issue fixed

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const allowedOrigins = ['http://localhost:3000', 'https://veero-ki-seva.vercel.app'];
+
 require('dotenv').config();  // for .env file
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 app.use(express.json());  // Parses incoming JSON
 app.use('/uploads', express.static('uploads'));
 
